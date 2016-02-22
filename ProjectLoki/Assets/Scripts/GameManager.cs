86d0dee065f742518dbc98397ebd818241e87ea4
Chris <nearman.cj@gmail.com>
2016-02-21 18:51:18 -0800

    Added tests for photon connection.

---
+++ ProjectLoki/Assets/Scripts/GameManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class GameManager : MonoBehaviour
+{
+    void OnJoinedRoom()
+    {
+        PhotonNetwork.Instantiate("Player", Vector3.zero, Quaternion.identity, 0);  
+    }       
+}
