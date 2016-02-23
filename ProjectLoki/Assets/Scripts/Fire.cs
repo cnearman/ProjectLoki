efdd64ed6217bc22fc3cc6783fba69d9f2cb8a82
Chris <nearman.cj@gmail.com>
2016-02-22 22:47:37 -0800

    Added firing distribution and Default distributions.

---
+++ ProjectLoki/Assets/Scripts/Fire.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace ProjectLoki.Weapons
+{
+    public class Fire
+    {
+        public Fire() { }
+
+        public Fire(Vector3 pos, Vector3 rot)
+        {
+            this.Position = pos;
+            this.Rotation = rot;
+        }
+
+        public Vector3 Position { get; set; }
+
+        public Vector3 Rotation { get; set; }
+    }
+}
